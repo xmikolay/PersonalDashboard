9ac2747e52a6ed871e8ecb251a8411b89d00061a
Mikolaj Makoszewski <S00254903@atu.ie>
2025-08-27 20:35:26 +0100

    Updated ToDoService and TaskItems to support inputting and viewing tasks

--- PersonalDashboard/Models/TaskItems.cs
+++ PersonalDashboard/Models/TaskItems.cs
@@ -3,12 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace PersonalDashboard.Models
 {
-    public class TaskItems
+    public class TaskItems : INotifyPropertyChanged
     {
-        public string Description { get; set; }
-        public bool isCompleted { get; set; }
+        private string _description;
+        private bool _isCompleted;
+
+        //Task text that user inputs
+        public string Description 
+        { 
+            get => _description ;
+            set { _description = value; OnPropertyChanged(); } 
+        }
+
+        //Whether task is completed or not
+        public bool isCompleted 
+        { 
+            get => _isCompleted; 
+            set { _isCompleted = value; OnPropertyChanged(); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
--- PersonalDashboard/Services/ToDoService.cs
+++ PersonalDashboard/Services/ToDoService.cs
@@ -3,10 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using PersonalDashboard.Models;
 
 namespace PersonalDashboard.Services
 {
-    internal class ToDoService
+    /// <summary>
+    /// Handles data operations for To-Do tasks, such as loading and saving tasks to a file.
+    /// </summary>
+    public class ToDoService
     {
+        private readonly string _folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "PersonalDashboard");
+        private readonly string _filePath;
+
+        public ToDoService()
+        {
+            _filePath = Path.Combine(_folderPath, "tasks.json");
+        }
+
+        public List<TaskItems> LoadTasks()
+        {
+            try
+            {
+                if (!File.Exists(_filePath))
+                    return new List<TaskItems>();   //nothing saved yet
+
+                var json = File.ReadAllText(_filePath);
+                var tasks = JsonConvert.DeserializeObject<List<TaskItems>>(json);
+
+                return tasks ?? new List<TaskItems>();
+            }
+            catch
+            {
+                //fail with empty list incase of fatal error with read or json
+                return new List<TaskItems>();
+            }
+               
+        }
+
+        public void SaveTasks(IEnumerable<TaskItems> tasks)
+        {
+            try
+            {
+                if (!Directory.Exists(_folderPath))
+                    Directory.CreateDirectory(_folderPath);
+
+                var json = JsonConvert.SerializeObject(tasks, Formatting.Indented);
+                File.WriteAllText(_filePath, json);
+            }
+            catch
+            {
+                //fail silently for now
+            }
+        }
     }
 }
